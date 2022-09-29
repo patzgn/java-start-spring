f39224033d0e8101e83eb251074130fefb72d8eb
patzgn <patrycja.zgierun@gmail.com>
2022-09-29 20:38:27 +0200

    defining entities

---
+++ entitymanager-and-repositories/defining-entities/src/main/java/com/example/definingentities/BikerentApplication.java
@@ -0,0 +1,13 @@
+package com.example.definingentities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikerentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikerentApplication.class, args);
+	}
+
+}
