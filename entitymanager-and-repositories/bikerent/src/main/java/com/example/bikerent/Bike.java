fae71e1a01e803a37baa9113e753dc6dba259d23
patzgn <patrycja.zgierun@gmail.com>
2022-09-29 22:17:21 +0200

    repository added

--- entitymanager-and-repositories/bikerent/src/main/java/com/example/bikerent/Bike.java
+++ entitymanager-and-repositories/bikerent/src/main/java/com/example/bikerent/Bike.java
@@ -15,4 +15,24 @@ public class Bike {
 
     public Bike() {
     }
+
+    public Bike(Long id, String model, String serialNo, double hourPrice, double dayPrice) {
+        this.id = id;
+        this.model = model;
+        this.serialNo = serialNo;
+        this.hourPrice = hourPrice;
+        this.dayPrice = dayPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", serialNo='" + serialNo + '\'' +
+                ", hourPrice=" + hourPrice +
+                ", dayPrice=" + dayPrice +
+                ", borrowerId='" + borrowerId + '\'' +
+                '}';
+    }
 }
