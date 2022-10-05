f4452020cca7908626c2d9274a192dd5a8f1c974
patzgn <patrycja.zgierun@gmail.com>
2022-10-05 19:17:38 +0200

    mvc controller

---
+++ mvc-controller/src/main/java/com/example/mvccontroller/example/ExampleController.java
@@ -0,0 +1,20 @@
+package com.example.mvccontroller.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ExampleController {
+    @RequestMapping("/first")
+    @ResponseBody
+    String first() {
+        return "Tekst z metody first()";
+    }
+
+    @RequestMapping("/second")
+    @ResponseBody
+    String second() {
+        return "Tekst z metody second()";
+    }
+}
---
+++ mvc-controller/src/test/java/com/example/mvccontroller/MvcControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
