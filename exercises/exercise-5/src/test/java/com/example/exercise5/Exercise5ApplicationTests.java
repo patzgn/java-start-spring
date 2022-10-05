99fb3a1d8f07e9a4ba8c613f615ea8f7ec762ba3
patzgn <103074854+patzgn@users.noreply.github.com>
2022-10-06 00:24:11 +0200

    exercise 5 added

---
+++ exercises/exercise-5/src/test/java/com/example/exercise5/Exercise5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
