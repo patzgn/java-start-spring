30382d187ef99b7c5586fc6cc9527c58cf39e06b
patzgn <103074854+patzgn@users.noreply.github.com>
2022-10-06 09:55:31 +0200

    mvc pattern

---
+++ mvc-pattern/src/main/java/com/example/mvcpattern/MvcPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcPatternApplication.class, args);
+	}
+
+}
---
+++ mvc-pattern/src/main/java/com/example/mvcpattern/RandomNumberService.java
@@ -0,0 +1,16 @@
+package com.example.mvcpattern;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class RandomNumberService {
+    private final Random random = new Random();
+
+    int generateRandomInRange(int start, int end) {
+        int range = end - start + 1;
+        int randomNumber = random.nextInt(range);
+        return start + randomNumber;
+    }
+}
