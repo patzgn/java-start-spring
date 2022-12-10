05657e25075f78cececc2615b166ef1261afe3f7
patzgn <103074854+patzgn@users.noreply.github.com>
2022-12-10 16:51:16 +0100

    validation config

---
+++ 06-validation/01-config/src/main/java/com/example/config/PersonService.java
@@ -0,0 +1,50 @@
+package com.example.config;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PersonService {
+
+    private final Set<Person> people = new HashSet<>();
+    private final Validator validator;
+
+    @Autowired
+    public PersonService(Validator validator) {
+        this.validator = validator;
+    }
+
+    public void addPerson(Person person) {
+        Set<ConstraintViolation<Object>> errors = validator.validate(person);
+        if (!errors.isEmpty()) {
+            System.out.println(">Obiekt nie może być dodany, lista błędów");
+            errors.forEach(err ->
+                    System.out.printf(">>>%s %s (%s)\n",
+                            err.getPropertyPath(),
+                            err.getMessage(),
+                            err.getInvalidValue())
+            );
+        } else {
+            people.add(person);
+            System.out.println(">Obiekt został dodany");
+        }
+    }
+
+    public Set<Person> getPeople() {
+        return people;
+    }
+
+    @Override
+    public String toString() {
+        String result = "People size: " + people.size() + "\n";
+        for (Person person : people) {
+            result = result + person + "\n";
+        }
+        return result;
+    }
+}
