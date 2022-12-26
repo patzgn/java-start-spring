90a7455ae122f653b6bc33a8e55aaaa81b2f6bb3
patzgn <103074854+patzgn@users.noreply.github.com>
2022-12-27 00:14:10 +0100

    api config

---
+++ 05-api/01-config/src/main/java/com/example/config/BookController.java
@@ -0,0 +1,29 @@
+package com.example.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @GetMapping
+    List<Book> getAllBooks() {
+        return List.of(
+                new Book("Harry Potter", "J.K. Rowling", 1997, 223),
+                new Book("Cień wiatru", "Carlos Ruiz Zafon", 2001,565),
+                new Book("Latarnik", "Henryk Sienkiewicz", 1881, 24)
+        );
+    }
+
+    @GetMapping("/1")
+    Book getSingleBook() {
+        return new Book("Harry Potter", "J.K. Rowling", 1997, 223);
+    }
+
+}
