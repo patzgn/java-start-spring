1df984364d23b796844c4d97c05f4b36c77af2ed
patzgn <103074854+patzgn@users.noreply.github.com>
2022-12-27 00:41:35 +0100

    api params

---
+++ 05-api/03-params/src/main/java/com/example/params/ProductRepository.java
@@ -0,0 +1,41 @@
+package com.example.params;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+    private final List<Product> products = new ArrayList<>();
+
+    public ProductRepository() {
+        products.add(
+                new Product(1, "Mleko", 3.8, new Producer("Mleczex", new Address("Warszawa", "20-222", "Wysoka 20")))
+        );
+        products.add(
+                new Product(2, "Czekolada mleczna", 4.2, new Producer("Czokoszok", new Address("Wrocław", "50-123", "Kucharska 18")))
+        );
+        products.add(
+                new Product(3, "Kawa rozpuszczalna", 16.99, new Producer("Kaw-Pol", new Address("Kraków", "10-234", "Niska 33")))
+        );
+    }
+
+    List<Product> findAll() {
+        return products;
+    }
+
+    List<Product> findAllByName(String name) {
+        return products.stream()
+                .filter(p -> p.getName().equalsIgnoreCase(name))
+                .toList();
+    }
+
+    Product findById(int id) {
+        if (id > products.size()) {
+            return null;
+        } else {
+            return products.get(id - 1);
+        }
+    }
+}
