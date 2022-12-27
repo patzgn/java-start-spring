1c5462e2281fb7348933e17133f23a329f12a3d2
patzgn <103074854+patzgn@users.noreply.github.com>
2022-12-27 01:03:25 +0100

    api response

---
+++ 05-api/04-response/src/main/java/com/example/response/Producer.java
@@ -0,0 +1,27 @@
+package com.example.response;
+
+public class Producer {
+    private String name;
+    private Address address;
+
+    public Producer(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
---
+++ 05-api/04-response/src/main/java/com/example/response/ProductRepository.java
@@ -0,0 +1,42 @@
+package com.example.response;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductRepository {
+    private final List<Product> products = new ArrayList<>();
+
+    public ProductRepository() {
+        products.add(
+                new Product(1, "Mleko", 3.8, new Producer("Mleczex", new Address("Warszawa", "20-222", "Wysoka 20")))
+        );
+        products.add(
+                new Product(2, "Czekolada mleczna", 4.2, new Producer("Czokoszok", new Address("Wrocław", "50-123", "Kucharska 18")))
+        );
+        products.add(
+                new Product(3, "Kawa rozpuszczalna", 16.99, new Producer("Kaw-Pol", new Address("Kraków", "10-234", "Niska 33")))
+        );
+    }
+
+    List<Product> findAll() {
+        return products;
+    }
+
+    List<Product> findAllByName(String name) {
+        return products.stream()
+                .filter(p -> p.getName().equalsIgnoreCase(name))
+                .toList();
+    }
+
+    Optional<Product> findById(int id) {
+        if (id > products.size()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(products.get(id - 1));
+        }
+    }
+}
