5ce7f36bbe816974c2ea27abf2fad853b7dc961b
patzgn <103074854+patzgn@users.noreply.github.com>
2022-12-30 18:18:50 +0100

    spring security users db

---
+++ 07-spring-security/06-users-db/src/main/java/com/example/usersdb/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.usersdb.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ 07-spring-security/06-users-db/src/main/java/com/example/usersdb/user/UserService.java
@@ -0,0 +1,21 @@
+package com.example.usersdb.user;
+
+import com.example.usersdb.user.dto.UserCredentialsDto;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<UserCredentialsDto> findCredentialsByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .map(UserCredentialsDtoMapper::map);
+    }
+}
