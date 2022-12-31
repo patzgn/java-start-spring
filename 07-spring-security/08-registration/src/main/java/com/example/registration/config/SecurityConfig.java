4ff789548453c671cc7ab714977cd224e387a222
patzgn <103074854+patzgn@users.noreply.github.com>
2022-12-31 14:21:47 +0100

    spring security registration

---
+++ 07-spring-security/08-registration/src/main/java/com/example/registration/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.registration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/img/**", "/styles/**").permitAll()
+                .mvcMatchers("/register", "/confirmation").permitAll()
+                .mvcMatchers("/secured").hasAnyRole("USER", "ADMIN")
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+        http.csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
