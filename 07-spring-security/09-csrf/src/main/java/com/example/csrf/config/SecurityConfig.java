a2a32bb0c7a6a8fe1e3ceb621fc4fde6b343a315
patzgn <103074854+patzgn@users.noreply.github.com>
2022-12-31 18:23:42 +0100

    spring security csrf

---
+++ 07-spring-security/09-csrf/src/main/java/com/example/csrf/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.csrf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/img/**", "/styles/**").permitAll()
+                .mvcMatchers("/register", "/confirmation").permitAll()
+                .mvcMatchers("/secured", "/change-password").hasAnyRole("USER", "ADMIN")
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+        http.logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout/**", HttpMethod.GET.name()))
+                .logoutSuccessUrl("/")
+        );
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
---
+++ 07-spring-security/09-csrf/src/main/java/com/example/csrf/web/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.csrf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class LoginController {
+    @GetMapping("/login")
+    String loginForm() {
+        return "login-form";
+    }
+}
